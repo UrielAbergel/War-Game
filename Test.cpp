ff791a8a67e6b00289a35b8033ab0b89fce308da
urielabergel <myfliper2@gmail.com>
2020-05-20 20:13:31 +0300

    make all SOURCE AND HEADERS

--- Test.cpp
+++ Test.cpp
@@ -6,17 +6,8 @@
 using namespace WarGame;
 TEST_CASE("MINI GAME")
 {
-    Game::Game(): board (8, 8) {
-        // Add soldiers for player 1:
-        board[{0,1}] = new FootSoldier(1);
-        board[{0,3}] = new FootCommander(1);
-        board[{0,5}] = new FootSoldier(1);
-        // Add soldiers for player 2:
-        board[{7,1}] = new FootSoldier(2);
-        board[{7,3}] = new FootCommander(2);
-        board[{7,5}] = new FootSoldier(2);
-        // In your game, you can put more soldier types, such as the sniper and the paramedic types.
-    }
+
+
 }
 
 
